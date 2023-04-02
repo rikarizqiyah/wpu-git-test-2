5c6ce19e3f803c7ab35e4f467ac14851c125b72b
rikarizqiyah <rikaraudhotul@gmail.com>
2023-04-02 07:11:25 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quam saepe molestiae, eius aliquam recusandae, ab sed pariatur dolores odio optio alias officia libero enim? Hic omnis iste quam eius.</p>
 </body>
 </html>
